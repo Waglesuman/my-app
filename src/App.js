e72aa6d34a6b1e97e6dff2a24643e894ff58793a
Suman <sumanwagle7000@gmail.com>
2023-03-20 13:49:27 +0545

    excerpt updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import Moment from 'react-moment';
+
+function truncate(text, maxLength) {
+  const words = text.split(' ');
+  if (words.length > maxLength) {
+    return words.slice(0, maxLength).join(' ') + '...';
+  } else {
+    return text;
+  }
+}
 
 function BlogPosts() {
   const [posts, setPosts] = useState([]);
-
+  
+  /* A React Hook that is used to fetch data from the WordPress API. */
   useEffect(() => {
-    axios.get('http://colormag.local/wp-json/wp/v2/posts?per_page=5')
+    axios.get('http://colormag.local/wp-json/wp/v2/posts?per_page=10')
       .then(response => {
         setPosts(response.data);
       })
@@ -15,13 +26,27 @@ function BlogPosts() {
   }, []);
 
   return (
-    <div>
+    <div className='container mt-2'>
       <h1>Latest Blog Posts</h1>
+     {/* A JavaScript function that is used to loop through the posts array and display the data.  */}
       {posts.map(post => (
-        <div key={post.id}>
+        <div key={post.id} className='card border-dark mt-3 mb-3'>
+
+        {/* Header or Title */}
+        <div className='card-header' >
           <h2>{post.title.rendered}</h2>
-          <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
-          <p dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }} />
+          </div>
+          {/* Body */}
+          <div className='card-body'>
+          {/* <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} /> */}
+          <p dangerouslySetInnerHTML={{ __html: truncate(post.content.rendered, 100)}} />
+          </div>
+
+          {/* Footer */}
+          <div className='card-footer'> 
+          <Moment fromNow> {post.date} </Moment>
+          <div className='btn btn-success float-end'>Read more...</div>
+          </div>
         </div>
       ))}
     </div>
